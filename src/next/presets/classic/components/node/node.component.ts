95685e17cb062a11f1fdab2a8f95f03c7955caac
Ni55aN <huccah@gmail.com>
2022-11-17 19:55:29 +0200

    sort inputs/controls/outputs by index

--- src/next/presets/classic/components/node/node.component.ts
+++ src/next/presets/classic/components/node/node.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, ChangeDetectionStrategy, HostBinding, ElementRef, OnInit, ChangeDetectorRef, OnChanges } from '@angular/core';
 import { ClassicPreset } from 'rete';
 import { Directive } from '@angular/core';
+import { KeyValue } from '@angular/common';
 
 @Directive({
   selector: '[refComponent]'
@@ -39,5 +40,12 @@ export class NodeComponent implements OnChanges {
     this.cdr.detectChanges()
     requestAnimationFrame(() => this.rendered())
     this.seed++ // force render sockets
+  }
+
+  sortByIndex<N extends object, T extends KeyValue<string, N & { index?: number }>>(a: T, b: T) {
+    const ai = a.value.index || 0
+    const bi = b.value.index || 0
+
+    return ai - bi
   }
 }
