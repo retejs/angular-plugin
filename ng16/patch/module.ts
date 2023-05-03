fd594a6bb6a058d8eaa4477173ac3f4acb8e33c0
Ni55aN <huccah@gmail.com>
2023-05-04 02:19:36 +0300

    support angular 16

---
+++ ng16/patch/module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NodeComponent, RefDirective } from './presets/classic/components/node/node.component';
+import { ConnectionComponent } from './presets/classic/components/connection/connection.component';
+import { SocketComponent } from './presets/classic/components/socket/socket.component';
+import { ConnectionWrapperComponent } from './presets/classic/components/connection/connection-wrapper.component';
+import { ControlComponent } from './presets/classic/components/control/control.component';
+
+@NgModule({
+  declarations: [
+    RefDirective,
+    NodeComponent,
+    ConnectionComponent,
+    ConnectionWrapperComponent,
+    SocketComponent,
+    ControlComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    RefDirective,
+    NodeComponent,
+    ConnectionComponent,
+    ConnectionWrapperComponent,
+    SocketComponent,
+    ControlComponent
+  ]
+})
+export class ReteModule {}
---
+++ ng16/patch/presets/minimap/module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MinimapComponent } from './components/minimap/minimap.component';
+import { MiniViewportComponent } from './components/mini-viewport/mini-viewport.component';
+import { MiniNodeComponent } from './components/mini-node/mini-node.component';
+
+@NgModule({
+  declarations: [
+    MinimapComponent,
+    MiniViewportComponent,
+    MiniNodeComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MinimapComponent,
+    MiniViewportComponent,
+    MiniNodeComponent
+  ]
+})
+export class ReteMinimapModule {}
