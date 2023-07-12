7d7b9f77fa1357d93cf1ef01f44f4aeb0c0b0356
Ni55aN <huccah@gmail.com>
2023-07-12 18:10:15 +0300

    remove unused files

--- dev.sh
+++
@@ -1,4 +0,0 @@
-# npm run build && cd dist && npm pack && mv rete-angular-plugin-2.0.0-beta.1.tgz .. && cd .. && npm i --prefix ../playground-angular rete-angular-plugin-2.0.0-beta.1.tgz -f
-# DEST=../playground-angular/node_modules/rete-angular-plugin npm run build:dev
-
-nodemon --watch src --ext ts,html,sass --exec "npm run build && cd dist && npm pack && mv rete-angular-plugin-2.0.0-beta.1.tgz .. && cd .. && npm i --prefix ../playground-angular rete-angular-plugin-2.0.0-beta.1.tgz -f"
